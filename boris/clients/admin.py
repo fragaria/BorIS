ce9dd2095fe68e6cde943d23bb0453296592d87a
xaralis <filip.varecha@fragaria.cz>
2011-10-01 10:31:40 -0400

    Anamnesis add interface works (refs #19)

--- boris/clients/admin.py
+++ boris/clients/admin.py
@@ -4,17 +4,19 @@ from anyjson import serialize
 
 from django.conf.urls.defaults import patterns, url
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.forms import Textarea
 from django.forms.extras.widgets import SelectDateWidget
-from django.http import Http404, HttpResponseBadRequest, HttpResponse
+from django.http import Http404, HttpResponseBadRequest, HttpResponse, HttpResponseRedirect
 from django.utils.translation import ugettext as _
 from django.utils.dateformat import format
 from django.utils.formats import get_format
+from django.utils.encoding import force_unicode
+from django.utils.html import escape, escapejs
 
 from boris.clients.models import Client, Drug, ClientNote, Town,\
     RiskyBehavior, Anamnesis, DrugUsage, RiskyManners
-from django.core.urlresolvers import reverse
 from boris.clients.forms import ReadOnlyWidget
 
 class DrugUsageInline(admin.StackedInline):
@@ -78,7 +80,43 @@ class AnamnesisAdmin(admin.ModelAdmin):
             kwargs['initial'] = cid
             return db_field.formfield(**kwargs)
         else:
-            return super(AnamnesisAdmin, self).formfield_for_dbfield(db_field, **kwargs) 
+            return super(AnamnesisAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+        
+    def response_add(self, request, obj, post_url_continue='../%s/'):
+        """
+        Determines the HttpResponse for the add_view stage.
+        """
+        opts = obj._meta
+        pk_value = obj._get_pk_val()
+
+        msg = _('The %(name)s "%(obj)s" was added successfully.') % {'name': force_unicode(opts.verbose_name), 'obj': force_unicode(obj)}
+        # Here, we distinguish between different save types by checking for
+        # the presence of keys in request.POST.
+        if "_continue" in request.POST:
+            self.message_user(request, msg + ' ' + _("You may edit it again below."))
+            if "_popup" in request.POST:
+                post_url_continue += "?_popup=1"
+            return HttpResponseRedirect(post_url_continue % pk_value)
+
+        if "_popup" in request.POST:
+            # @attention: Change function call
+            return HttpResponse('<script type="text/javascript">opener.dismissAddAnamnesisPopup(window, "%s", "%s");</script>' % \
+                # escape() calls force_unicode.
+                (escape(pk_value), escapejs(obj)))
+        elif "_addanother" in request.POST:
+            self.message_user(request, msg + ' ' + (_("You may add another %s below.") % force_unicode(opts.verbose_name)))
+            return HttpResponseRedirect(request.path)
+        else:
+            self.message_user(request, msg)
+
+            # Figure out where to redirect. If the user has change permission,
+            # redirect to the change-list page for this object. Otherwise,
+            # redirect to the admin index.
+            if self.has_change_permission(request, None):
+                post_url = '../'
+            else:
+                post_url = '../../../'
+            return HttpResponseRedirect(post_url)
         
 
 class ClientAdmin(admin.ModelAdmin):
