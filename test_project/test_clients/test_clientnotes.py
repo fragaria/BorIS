af0868a7bc59073f90ff59516d28a6c314dfe87c
Hynek Urban <hynek.urban@fragaria.cz>
2011-10-03 17:48:56 +0200

    Test code reformat.

--- test_project/test_clients/test_clientnotes.py
+++ test_project/test_clients/test_clientnotes.py
@@ -9,40 +9,35 @@ from test_project.helpers import get_testing_user, get_testing_client
 
 class TestClientNote(HttpTestCase):
 
-   def setUp(self):
+    def setUp(self):
 
-       self.my_client = get_testing_client()
-       self.user = get_testing_user()
+        self.my_client = get_testing_client()
+        self.user = get_testing_user()
 
+    def post_note(self):
+        return self.client.post(reverse('admin:clients_add_note', kwargs={'object_id': self.my_client.pk}),
+            data = {'text': 'Note text.'},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+        )
 
-   def test_add_ok(self):
-       self.client.login(username=self.user.username, password=self.user.cleartext_password)
-       res = self.client.post(reverse('admin:clients_add_note', kwargs={'object_id': self.my_client.pk}),
-           data = {'text': 'Note text.'},
-           HTTP_X_REQUESTED_WITH='XMLHttpRequest'
-       )
+    def test_add_ok(self):
+        self.client.login(username=self.user.username, password=self.user.cleartext_password)
+        res = self.post_note()
 
-       self.assert_equals(200, res.status_code)
+        self.assert_equals(200, res.status_code)
 
+    def test_add_actually_adds(self):
 
-   def test_add_actually_adds(self):
+        self.assert_equals(0, ClientNote.objects.count())
+        self.client.login(username=self.user.username, password=self.user.cleartext_password)
+        for i in range(0, 10):
+            self.post_note()
+        self.assert_equals(10, ClientNote.objects.count())
 
-       self.assert_equals(0, ClientNote.objects.count())
-       self.client.login(username=self.user.username, password=self.user.cleartext_password)
-       for i in range(0, 10):
-           self.client.post(reverse('admin:clients_add_note', kwargs={'object_id': self.my_client.pk}),
-               data = {'text': 'Note text.'},
-               HTTP_X_REQUESTED_WITH='XMLHttpRequest'
-           )
-       self.assert_equals(10, ClientNote.objects.count())
+    def test_restricted_access(self):
+        # login ommitted
+        self.assert_equals(0, ClientNote.objects.count())
+        self.post_note()
 
-
-   def test_restricted_access(self):
-       # login ommitted
-       self.assert_equals(0, ClientNote.objects.count())
-       self.client.post(reverse('admin:clients_add_note', kwargs={'object_id': self.my_client.pk}),
-           data = {'text': 'Note text.'},
-           HTTP_X_REQUESTED_WITH='XMLHttpRequest'
-       )
-       self.assert_equals(0, ClientNote.objects.count())
+        self.assert_equals(0, ClientNote.objects.count())
 
