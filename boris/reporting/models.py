847c1677b503848bd6392f433c0734ab8265001c
Hynek Urban <hynek.urban@fragaria.cz>
2011-12-08 21:45:23 +0100

    Disable ON DELETE CASCADE for the DB view.

--- boris/reporting/models.py
+++ boris/reporting/models.py
@@ -3,17 +3,19 @@ from django.db import models
 from boris.clients.models import Person, Town, Drug
 from boris.services.models.core import Service, Encounter
 
+DO_NOTHING = models.DO_NOTHING
+
 class SearchEncounter(models.Model):
     """
     An augmented model corresponding to a database view.
     """
-    person = models.ForeignKey(Person, related_name='+')
-    town = models.ForeignKey(Town, related_name='+')
+    person = models.ForeignKey(Person, related_name='+', on_delete=DO_NOTHING)
+    town = models.ForeignKey(Town, related_name='+', on_delete=DO_NOTHING)
     is_client = models.BooleanField()
     is_anonymous = models.BooleanField()
     is_practitioner = models.BooleanField()
     client_sex = models.PositiveSmallIntegerField()
-    primary_drug = models.ForeignKey(Drug, related_name='+')
+    primary_drug = models.ForeignKey(Drug, related_name='+', on_delete=DO_NOTHING)
     primary_drug_usage = models.PositiveSmallIntegerField()
     month = models.SmallIntegerField()
     year = models.SmallIntegerField()
@@ -22,11 +24,11 @@ class SearchEncounter(models.Model):
         managed = False
 
 class SearchService(models.Model):
-    service = models.ForeignKey(Service, related_name='+')
-    person = models.ForeignKey(Person, related_name='+')
-    encounter = models.ForeignKey(Encounter, related_name='+')
+    service = models.ForeignKey(Service, related_name='+', on_delete=DO_NOTHING)
+    person = models.ForeignKey(Person, related_name='+', on_delete=DO_NOTHING)
+    encounter = models.ForeignKey(Encounter, related_name='+', on_delete=DO_NOTHING)
     content_type_model = models.CharField(max_length=255)
-    town = models.ForeignKey(Town, related_name='+')
+    town = models.ForeignKey(Town, related_name='+', on_delete=DO_NOTHING)
     month = models.SmallIntegerField()
     year = models.SmallIntegerField()
 
