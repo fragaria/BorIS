77bfb8a539c6a9df0048ab6e702d9c99d5120668
Hynek Urban <hynek.urban@fragaria.cz>
2011-12-26 14:07:20 +0100

    No need to wrap views twice (refs #43).

--- boris/reporting/admin.py
+++ boris/reporting/admin.py
@@ -26,10 +26,10 @@ class ReportingInterfaceTab(object):
 
     def get_title(self):
         return self.report.title
-    
+
     def get_absolute_url(self):
         return reverse(self.get_urlname())
-    
+
 
 def interfacetab_factory(ReportClass, FormClass):
     return type(ReportClass.__name__ + 'Tab', (ReportingInterfaceTab,), {
@@ -39,8 +39,8 @@ class ReportingInterface(object):
     """
     Separate report forms are splitted to tabs in admin, this
     class handles management of these forms.
-    
-    Tabs are defined as ReportInterfaceTab subclasses listed in 
+
+    Tabs are defined as ReportInterfaceTab subclasses listed in
     `tabs` attribute.
     """
     tabs = (
@@ -52,11 +52,11 @@ class ReportingInterface(object):
 
 class ReportingInterfaceHandler(object):
     """Class-based view for showing reporting interface."""
-    
+
     def __call__(self, request, tab_class=None):
         interface = ReportingInterface()
         tabs = {}
-        
+
         for t in interface.tabs:
             tab = t()
             if tab_class == t and request.method == 'POST':
@@ -66,37 +66,30 @@ class ReportingInterfaceHandler(object):
             else:
                 form = tab.form()
             tabs[tab] = form
-            
+
         ctx = {'tabs': tabs, 'interface': interface}
         return render(request, 'reporting/interface.html', ctx)
-    
+
     def get_urls(self):
         """
         Returns all urls for interface. Each tab has it's own POST URL plus
         there is one extra URL for base view.
         """
-        def wrap(view, cacheable=False):
-            """
-            Utility to make our functions look like ModelAdmin bound ones.
-            This ensures that log-in related stuff will work as expected
-            and only admin users will be able to call our views.
-            """
-            def wrapper(*args, **kwargs):
-                return admin.site.admin_view(view, cacheable)(*args, **kwargs)
-            return update_wrapper(wrapper, view)
-        
+
         interface = ReportingInterface()
-        
+
         urlpatterns = patterns('',
-            url('^$', wrap(self.__call__, cacheable=True), name='reporting_base')
+            url('^$', admin.site.admin_view(self.__call__, cacheable=True),
+                name='reporting_base')
         )
-        
+
         for t in interface.tabs:
             urlpatterns += patterns('',
-                url(r'^%s/$' % slugify(t.__name__), wrap(self.__call__, cacheable=False),
+                url(r'^%s/$' % slugify(t.__name__), admin.site.admin_view(
+                            self.__call__, cacheable=False),
                     kwargs={'tab_class': t}, name=t.get_urlname())
             )
-        
+
         return urlpatterns, 'reporting', None
     urls = property(get_urls)
 
