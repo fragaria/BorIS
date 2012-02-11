916c7a90e08845030bd97e5528976ec629d9d1a5
Hynek Urban <hynek.urban@fragaria.cz>
2012-02-11 19:38:29 +0100

    Make the list view of SyringeCollection admin nicer (refs #57).

--- boris/other/admin.py
+++ boris/other/admin.py
@@ -1,6 +1,13 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
 from boris.other.models import SyringeCollection
 
+def user_list(self, obj):
+    return u'<br />'.join([unicode(s) for s in obj.persons.all()])
+user_list.short_description = _(u'Kdo')
+user_list.allow_tags = True
 
 class SyringeCollectionAdmin(admin.ModelAdmin):
 
@@ -9,9 +16,13 @@ class SyringeCollectionAdmin(admin.ModelAdmin):
         'fk': ['town',],
         'm2m': ['persons',],
     }
+    list_display = ('date', 'town', 'location', 'count', 'user_list')
+    list_filter = ('date', 'town',)
 
     class Meta:
         model = SyringeCollection
+
+SyringeCollectionAdmin.user_list = user_list
 
 
 admin.site.register(SyringeCollection, SyringeCollectionAdmin)
