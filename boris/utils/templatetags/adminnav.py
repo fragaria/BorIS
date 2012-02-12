7f08b6d003b17e13d4f68c3520ded9936af80618
xaralis <filip.varecha@fragaria.cz>
2012-02-12 10:01:04 -0500

    Highlight currently active menu record

---
+++ boris/utils/templatetags/adminnav.py
@@ -0,0 +1,35 @@
+'''
+Created on 12.2.2012
+
+@author: xaralis
+'''
+from django import template
+
+register = template.Library()
+
+@register.tag
+def active(parser, token):
+    args = token.split_contents()
+    template_tag = args[0]
+    if len(args) < 2:
+        raise template.TemplateSyntaxError, "%r tag requires at least one argument" % template_tag
+    return NavSelectedNode(args[1:])
+
+class NavSelectedNode(template.Node):
+    def __init__(self, urls):
+        self.urls = urls
+
+    def render(self, context):
+        path = context['request'].path
+
+        for url in self.urls:
+            if '"' not in url:
+                cpath = template.Variable(url).resolve(context)
+            else:
+                cpath = url.strip('"')
+
+            if (cpath == '/' or cpath == '') and not (path == '/' or path == ''):
+                return ""
+            if path.startswith(cpath):
+                return 'active'
+        return ""
