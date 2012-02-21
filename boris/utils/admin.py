60286b12c32e49118779b0266668b7c135e7ba3d
Hynek Urban <hynek.urban@fragaria.cz>
2012-02-21 15:22:21 +0100

    Do not show list_actions in pop-up windows (refs #60).

--- boris/utils/admin.py
+++ boris/utils/admin.py
@@ -4,12 +4,13 @@ Created on 12.2.2012
 @author: xaralis
 '''
 from django.contrib.admin.options import ModelAdmin
+from django.contrib.admin.views.main import ChangeList
 from django.utils.translation import ugettext_lazy as _
 
 def textual(title, ordering_field=None):
     """
     Fallbacks textual field to '---' instead of (None) in admin.
-    
+
     Sets proper title using ``title`` and if specified also ``admin_order_field``
     using ``ordering_field`` attribute.
     """
@@ -28,15 +29,30 @@ def textual(title, ordering_field=None):
     return decorator
 
 
+class BorisChangeList(ChangeList):
+    """Change list_display with respect to request."""
+
+    remove_in_popup = ('actions_display',)
+
+    def __init__(self, request, *args, **kwargs):
+        super(BorisChangeList, self).__init__(request, *args, **kwargs)
+        if 'pop' in request.GET: # do not display the action buttons in popups
+            self.list_display = filter(lambda x: x not in self.remove_in_popup,
+                self.list_display)
+
+
 class BorisBaseAdmin(ModelAdmin):
     """
-    Adds ``list_actions`` to simplify addition of actions to changelist rows. 
+    Adds ``list_actions`` to simplify addition of actions to changelist rows.
     """
     list_actions = ('change_link',)
 
     def __init__(self, *args, **kwargs):
         self.list_display += ('actions_display',)
         super(BorisBaseAdmin, self).__init__(*args, **kwargs)
+
+    def get_changelist(self, request, **kwargs):
+        return BorisChangeList
 
     def actions_display(self, obj):
         return ''.join(getattr(self, a)(obj) for a in self.list_actions)
