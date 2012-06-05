0e5485d6296ec06c245e1a78f83a113d681b679a
Hynek Urban <hynek.urban@fragaria.cz>
2012-06-05 22:09:34 +0200

    Make a help message more specific (refs #86).

--- boris/clients/admin.py
+++ boris/clients/admin.py
@@ -100,7 +100,7 @@ class AnamnesisAdmin(BorisBaseAdmin):
     def client__sex(self, obj):
         if obj.pk:
             return obj.client.get_sex_display()
-        return _(u'(Dostupné po uložení)')
+        return _(u'(Dostupné po uložení anamnézy)')
     client__sex.short_description = _(u'Pohlaví')
 
     def client__birthyear(self, obj):
@@ -109,7 +109,7 @@ class AnamnesisAdmin(BorisBaseAdmin):
         elif obj.pk:
             return _(u'(Zatím neznámý)')
         else:
-            return _(u'(Dostupné po uložení)')
+            return _(u'(Dostupné po uložení anamnézy)')
     client__birthyear.short_description = _(u'Rok narození')
 
     def formfield_for_dbfield(self, db_field, **kwargs):
