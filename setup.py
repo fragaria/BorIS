0da8ee587dbc3457997d23088ac0e73303b5b1ce
xaralis <filip.varecha@fragaria.cz>
2012-07-23 15:27:47 +0200

    Licensing

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
+"""
+Copyright (c) 2012 Filip Vařecha, Hynek Urban
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+"""
+
 from setuptools import setup, find_packages
 import boris
 
@@ -11,7 +33,7 @@ setup(
     )),
     author='BorIS team',
     author_email='info@boris.cz',
-    license='BSD',
+    license='MIT',
     url='http://boris.github.com/',
 
     packages=find_packages(
@@ -20,6 +42,8 @@ setup(
     ),
 
     include_package_data=True,
+
+    data_files=[("", ["LICENSE.txt"])],
 
     classifiers=[
         "Development Status :: 4 - Beta",
