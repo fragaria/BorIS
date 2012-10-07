c95044053c06c49476876874dc6b41e05b081dac
Hynek Urban <hynek.urban@fragaria.cz>
2012-10-07 13:16:55 +0200

    Avoid depencency problems upon clean syncdb + migrate.

--- boris/reporting/management.py
+++ boris/reporting/management.py
@@ -6,6 +6,8 @@ from south.signals import post_migrate
 from boris import reporting
 
 def install_views(app, **kwargs):
+    if app != 'services':
+        return # Avoid repeated runs and dependency problems.
     print "Installing reporting views ..."
     cursor = connection.cursor()
     sql_file = open(join(dirname(reporting.__file__), 'sql', 'reporting-views.mysql.sql'), 'r')
