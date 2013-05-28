eacb6b5356f0629d7b573cad37e9308ce7cd1e02
xaralis <filip.varecha@fragaria.cz>
2013-05-28 18:42:20 +0200

    Sign off users who are inactive for 30 minutes.

--- boris/settings/base.py
+++ boris/settings/base.py
@@ -125,6 +125,10 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # store session data in cache if possible
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
+# after 30 minutes of inactivity, user will be logged out
+SESSION_COOKIE_AGE = 60 * 30
+SESSION_SAVE_EVERY_REQUEST = True
+
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 # GRAPPELLI RELATED ------------------------------------------------------------
