80ad2451dabadeeeee6bb632ce6681fac3682b95
xaralis <filip.varecha@fragaria.cz>
2013-11-14 15:24:24 +0100

    Fix up logging on local.

--- boris/settings/local_template.py
+++ boris/settings/local_template.py
@@ -1,13 +1,25 @@
-from logging import LOGGING
-
 DEBUG = True
 
 # Don't log to sentry on local.
-LOGGING['root']['handlers'] = ['console']
-LOGGING['loggers']['django.request'] = {
-    'level': 'ERROR',
-    'handlers': ['console'],
-    'propagate': False,
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['console'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    }
 }
 
 TEMPLATE_LOADERS = (
