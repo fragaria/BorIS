8dee8186415262a7a22def049347e260ccb52bd4
Ondřej Chmelař <ondrej.chmelar@fragaria.cz>
2014-04-02 12:42:39 +0200

    added local.py for test settings

---
+++ test_boris/settings/local.py
@@ -0,0 +1,43 @@
+__author__ = 'ondrej'
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'test_boris', # Or path to database file if using sqlite3.
+        'USER': 'root', # Not used with sqlite3.
+        'PASSWORD': 'toor', # Not used with sqlite3.
+        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+        'OPTIONS': {
+            'charset': 'utf8',
+            "init_command": "SET storage_engine=INNODB",
+        },
+        'TEST_CHARSET': 'utf8',
+    }
+}
+
+INSTALLED_APPS = (
+    'grappelli',
+
+    # core django apps
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.redirects',
+    'django.contrib.sitemaps',
+    'django.contrib.staticfiles',
+
+    'boris.clients',
+    'boris.services',
+    'boris.reporting',
+    'boris.syringes',
+    'boris.utils',
+
+    'test_boris.test_services',
+)
+
