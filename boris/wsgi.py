6f7b5b77b1d2df95ad6898686ed6c1347a18c4c9
Ondřej Chmelař <ondrej.chmelar@fragaria.cz>
2015-05-15 17:27:56 +0200

    django 1.7 wsgi and json changes

--- boris/wsgi.py
+++ boris/wsgi.py
@@ -3,6 +3,6 @@ Created on 11.10.2011
 
 @author: xaralis
 '''
-import django.core.handlers.wsgi
 
-application = django.core.wsgi.get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
