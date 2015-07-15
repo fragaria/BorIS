15473c2ee0cfa181b01199b46893dfd07f993c67
Ondřej Chmelař <ondrej.chmelar@fragaria.cz>
2015-07-15 14:38:48 +0200

    add k-center services

---
+++ boris/services/migrations/0002_clotheswashing_communitywork_contactroom_groupcounselling_hygienicservice_internetusage_parentadviso.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('services', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ClothesWashing',
+            fields=[
+            ],
+            options={
+                'verbose_name': 'Pran\xed pr\xe1dla',
+                'proxy': True,
+                'verbose_name_plural': 'Pran\xed pr\xe1dla',
+            },
+            bases=('services.service',),
+        ),
+        migrations.CreateModel(
+            name='CommunityWork',
+            fields=[
+            ],
+            options={
+                'verbose_name': 'Obecn\u011b prosp\u011b\u0161n\xe9 pr\xe1ce',
+                'proxy': True,
+                'verbose_name_plural': 'Obecn\u011b prosp\u011b\u0161n\xe9 pr\xe1ce',
+            },
+            bases=('services.service',),
+        ),
+        migrations.CreateModel(
+            name='ContactRoom',
+            fields=[
+            ],
+            options={
+                'verbose_name': 'Kontaktn\xed m\xedstnost',
+                'proxy': True,
+                'verbose_name_plural': 'Kontaktn\xed m\xedstnosti',
+            },
+            bases=('services.service',),
+        ),
+        migrations.CreateModel(
+            name='GroupCounselling',
+            fields=[
+            ],
+            options={
+                'verbose_name': 'Skupinov\xe9 poradenstv\xed',
+                'proxy': True,
+                'verbose_name_plural': 'Skupinov\xe1 poradenstv\xed',
+            },
+            bases=('services.service',),
+        ),
+        migrations.CreateModel(
+            name='HygienicService',
+            fields=[
+            ],
+            options={
+                'verbose_name': 'Hygienick\xfd servis',
+                'proxy': True,
+                'verbose_name_plural': 'Hygienick\xe9 servisy',
+            },
+            bases=('services.service',),
+        ),
+        migrations.CreateModel(
+            name='InternetUsage',
+            fields=[
+            ],
+            options={
+                'verbose_name': 'Pou\u017eit\xed internetu klientem',
+                'proxy': True,
+                'verbose_name_plural': 'Pou\u017eit\xed internetu klientem',
+            },
+            bases=('services.service',),
+        ),
+        migrations.CreateModel(
+            name='ParentAdvisory',
+            fields=[
+            ],
+            options={
+                'verbose_name': 'Poradenstv\xed pro rodi\u010de',
+                'proxy': True,
+                'verbose_name_plural': 'Poradenstv\xed pro rodi\u010de',
+            },
+            bases=('services.service',),
+        ),
+        migrations.CreateModel(
+            name='PostUsage',
+            fields=[
+            ],
+            options={
+                'verbose_name': 'Po\u0161ta',
+                'proxy': True,
+                'verbose_name_plural': 'Po\u0161ty',
+            },
+            bases=('services.service',),
+        ),
+        migrations.CreateModel(
+            name='SocialServicesAgreement',
+            fields=[
+            ],
+            options={
+                'verbose_name': 'Uzav\u0159en\xed dohody o poskytov\xe1n\xed soci\xe1ln\xedch slu\u017eeb',
+                'proxy': True,
+                'verbose_name_plural': 'Uzav\u0159en\xed dohod o poskytov\xe1n\xed soci\xe1ln\xedch slu\u017eeb',
+            },
+            bases=('services.service',),
+        ),
+        migrations.CreateModel(
+            name='WorkTherapy',
+            fields=[
+            ],
+            options={
+                'verbose_name': 'Pracovn\xed terapie (samospr\xe1va)',
+                'proxy': True,
+                'verbose_name_plural': 'Pracovn\xed terapie (samospr\xe1va)',
+            },
+            bases=('services.service',),
+        ),
+        migrations.CreateModel(
+            name='WorkTherapyMeeting',
+            fields=[
+            ],
+            options={
+                'verbose_name': 'Sch\u016fzka pracovn\xed terapie (samospr\xe1vy)',
+                'proxy': True,
+                'verbose_name_plural': 'Sch\u016fzka pracovn\xed terapie (samospr\xe1vy)',
+            },
+            bases=('services.service',),
+        ),
+    ]
--- boris/services/models/basic.py
+++ boris/services/models/basic.py
@@ -364,3 +364,136 @@ class PhoneUsage(Service):
     class Options:
         codenumber = 15
         limited_to = ('Client',)
+
+class GroupCounselling(Service):
+    class Meta:
+        app_label = 'services'
+        proxy = True
+        verbose_name = _(u'Skupinové poradenství')
+        verbose_name_plural = _(u'Skupinová poradenství')
+
+    class Options:
+        codenumber = 16
+        limited_to = ('Client', )
+        available_in_modules = ['k', ]
+
+class ContactRoom(Service):
+    class Meta:
+        app_label = 'services'
+        proxy = True
+        verbose_name = _(u'Kontaktní místnost')
+        verbose_name_plural = _(u'Kontaktní místnosti')
+
+    class Options:
+        codenumber = 17
+        limited_to = ('Client', )
+        available_in_modules = ['k', ]
+
+class HygienicService(Service):
+    class Meta:
+        app_label = 'services'
+        proxy = True
+        verbose_name = _(u'Hygienický servis')
+        verbose_name_plural = _(u'Hygienické servisy')
+
+    class Options:
+        codenumber = 18
+        limited_to = ('Client', )
+        available_in_modules = ['k', ]
+
+
+class ClothesWashing(Service):
+    class Meta:
+        app_label = 'services'
+        proxy = True
+        verbose_name = _(u'Praní prádla')
+        verbose_name_plural = _(u'Praní prádla')
+
+    class Options:
+        codenumber = 19
+        limited_to = ('Client', )
+        available_in_modules = ['k', ]
+
+class InternetUsage(Service):
+    class Meta:
+        app_label = 'services'
+        proxy = True
+        verbose_name = _(u'Použití internetu klientem')
+        verbose_name_plural = _(u'Použití internetu klientem')
+
+    class Options:
+        codenumber = 20
+        limited_to = ('Client', )
+        available_in_modules = ['k', ]
+
+class WorkTherapy(Service):
+    class Meta:
+        app_label = 'services'
+        proxy = True
+        verbose_name = _(u'Pracovní terapie (samospráva)')
+        verbose_name_plural = _(u'Pracovní terapie (samospráva)')
+
+    class Options:
+        codenumber = 21
+        limited_to = ('Client', )
+        available_in_modules = ['k', ]
+
+class WorkTherapyMeeting(Service):
+    class Meta:
+        app_label = 'services'
+        proxy = True
+        verbose_name = _(u'Schůzka pracovní terapie (samosprávy)')
+        verbose_name_plural = _(u'Schůzka pracovní terapie (samosprávy)')
+
+    class Options:
+        codenumber = 22
+        limited_to = ('Client', )
+        available_in_modules = ['k', ]
+
+class CommunityWork(Service):
+    class Meta:
+        app_label = 'services'
+        proxy = True
+        verbose_name = _(u'Obecně prospěšné práce')
+        verbose_name_plural = _(u'Obecně prospěšné práce')
+
+    class Options:
+        codenumber = 23
+        limited_to = ('Client', )
+        available_in_modules = ['k', ]
+
+class ParentAdvisory(Service):
+    class Meta:
+        app_label = 'services'
+        proxy = True
+        verbose_name = _(u'Poradenství pro rodiče')
+        verbose_name_plural = _(u'Poradenství pro rodiče')
+
+    class Options:
+        codenumber = 24
+        limited_to = ('Client', )
+        available_in_modules = ['k', ]
+
+class PostUsage(Service):
+    class Meta:
+        app_label = 'services'
+        proxy = True
+        verbose_name = _(u'Pošta')
+        verbose_name_plural = _(u'Pošty')
+
+    class Options:
+        codenumber = 25
+        limited_to = ('Client', )
+        available_in_modules = ['k', ]
+
+class SocialServicesAgreement(Service):
+    class Meta:
+        app_label = 'services'
+        proxy = True
+        verbose_name = _(u'Uzavření dohody o poskytování sociálních služeb')  # The verbose_name of service is longer than 39 characters
+        verbose_name_plural = _(u'Uzavření dohod o poskytování sociálních služeb')
+
+    class Options:
+        codenumber = 26
+        limited_to = ('Client', )
+        available_in_modules = ['k', ]
