4679d8e6b6b5db2d4b398ea6c123c6b255705ebc
Ondřej Chmelař <ondrej.chmelar@fragaria.cz>
2015-08-18 15:17:27 +0200

    fixed migrations confict

---
+++ boris/clients/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('clients', '0003_groupcontact'),
+        ('clients', '0002_client_client_card'),
+    ]
+
+    operations = [
+    ]
