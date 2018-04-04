d05ffbdf739bac93f73d5d78e13da855ef9d322c
kdanek <32480541+kdanek@users.noreply.github.com>
2018-04-04 14:43:09 +0200

    14- fix spent time computation
    * _get_services_time fixed
    
    * debug prints removed

--- boris/reporting/reports/council.py
+++ boris/reporting/reports/council.py
@@ -244,12 +244,13 @@ class GovCouncilReport(BaseReport):
         if self.towns:
             filtering['encounter__where__in'] = self.towns
         sum = 0
-        content_types = []
+        service_encounters = []
         for service in self._get_services(Service):
+            service_encounter = [ service.content_type, service.encounter]
             # prevent double count in case of same service class being multiple on one encounter
-            if service.content_type not in content_types:
+            if service_encounter not in service_encounters:
                 sum += service.get_time_spent(filtering, get_indirect_content_types(), get_no_subservice_content_types())
-                content_types.append(service.content_type)
+                service_encounters.append(service_encounter)
         return sum
 
     # <--
