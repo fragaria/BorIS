fa33c5e54768e454cdd92ef5804d6c2f7833d722
Ondrej Chmelar <ondrej.chmelar@fragaria.cz>
2018-05-30 12:02:30 +0200

    38- speedup spent time computation (fix generated migration)

--- boris/services/migrations/0028_auto_20180530_1159.py
+++ boris/services/migrations/0028_auto_20180530_1159.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='indirectservice',
-            name='service_ptr',
-        ),
         migrations.DeleteModel(
             name='IndirectService',
         ),
